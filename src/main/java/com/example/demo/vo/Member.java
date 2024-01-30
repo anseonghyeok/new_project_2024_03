229d38a3618575be5cf89fbf35948711bff7ca6f
anseonghyeok <itnew0987@gmail.com>
2024-01-30 11:14:38 +0900

    회원 가입 후 가입한 회원의 정보를 화면에 표시

--- src/main/java/com/example/demo/vo/Member.java
+++ src/main/java/com/example/demo/vo/Member.java
@@ -8,15 +8,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Member {
-	int id;
-	String loginId;
-	String loginPw ;
-	String name;
-	String nickName ;
-	String cellphoneNum;
-	String email ;
-	
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private String loginId;
+	private String loginPw;
+	private int authLevel;
+	private String name;
+	private String nickname;
+	private String cellphoneNum;
+	private String email;
+	private boolean delStatus;
+	private String delDate;
 
-	}
-
-
+}
