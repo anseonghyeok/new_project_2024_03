218fba1840a94c58f86087f27aa823d6d4c4fdaf
anseonghyeok <itnew0987@gmail.com>
2024-01-31 15:17:22 +0900

    doWrite 할 때 작성자의 정보 저장, write,modify,delete 로그인 체크

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -47,7 +47,19 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public ResultData<Article> doWrite(HttpSession httpSession, String title, String body, int memberId) {
+	public ResultData<Article> doWrite(HttpSession httpSession, String title, String body) {
+
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요");
+		}
 
 		if (Ut.isNullOrEmpty(title)) {
 			return ResultData.from("F-1", "제목을 입력해주세요");
@@ -56,13 +68,7 @@ public class UsrArticleController {
 			return ResultData.from("F-2", "내용을 입력해주세요");
 		}
 
-		if (httpSession.getAttribute("loginedMemberId") == null) {
-			return ResultData.from("F-A", "로그인후 수정해주세요");
-		}
-
-		memberId = (int) httpSession.getAttribute("loginedMemberId");
-
-		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body, memberId);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title, body);
 
 		int id = (int) writeArticleRd.getData1();
 
@@ -75,17 +81,20 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Integer> doModify(HttpSession httpSession, int id, String title, String body) {
 
-		if (httpSession.getAttribute("loginedMemberId") == null) {
-			return ResultData.from("F-A", "로그인후 수정해주세요");
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
 		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요");
+		}
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
-		}
-
-		if ((int) httpSession.getAttribute("loginedMemberId") != article.getMemberId()) {
-			return ResultData.from("F-A", Ut.f("%d번 글은 권한이 없습니다", id), id);
 		}
 
 		articleService.modifyArticle(id, title, body);
@@ -93,27 +102,24 @@ public class UsrArticleController {
 		return ResultData.from("S-1", Ut.f("%d번 글을 수정했습니다", id), id);
 	}
 
-	private Object getAttribute(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public ResultData<Integer> doDelete(HttpSession httpSession, int id) {
 
-		if (httpSession.getAttribute("loginedMemberId") == null) {
-			return ResultData.from("F-A", "로그인후 삭제해주세요");
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+
+		if (isLogined == false) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요");
 		}
 
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
-		}
-
-		if ((int) httpSession.getAttribute("loginedMemberId") != article.getMemberId()) {
-			return ResultData.from("F-A", Ut.f("%d번 글은 권한이 없습니다", id), id);
 		}
 
 		articleService.deleteArticle(id);
--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -17,9 +17,10 @@ public interface ArticleRepository {
 			article SET
 			regDate = NOW(),
 			updateDate = NOW(),
-			title = #{title}, `body` = #{body},memberId = #{memberId}
+			memberId = #{memberId},
+			title = #{title}, `body` = #{body}
 			""")
-	public void writeArticle(String title, String body,int memberId);
+	public void writeArticle(int memberId, String title, String body);
 
 	@Select("SELECT LAST_INSERT_ID()")
 	public int getLastInsertId();
